a141f76dad768b5148e020809e526c3c47685689
Elena Vuceljic <>
2019-02-04 18:23:40 +0100

    implemented share handyman

--- app/src/main/java/com/example/ve183011m/pki_ve183011m/presentation/buyer/search/HandymanPreviewActivity.java
+++ app/src/main/java/com/example/ve183011m/pki_ve183011m/presentation/buyer/search/HandymanPreviewActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -58,6 +59,12 @@ public class HandymanPreviewActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.handyman_preview_menu, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
@@ -66,7 +73,20 @@ public class HandymanPreviewActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_share) {
+            share();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void share() {
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, handyman.getFullName());
+
+        startActivity(sharingIntent);
     }
 
     public void addNewRequest() {
